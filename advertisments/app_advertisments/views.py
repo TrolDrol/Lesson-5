4f775dce8de9f3b19d44b06141c31882a9da1b48
Азебан <yushurup@gmail.com>
2023-08-15 13:44:31 +0300

    'Homework_5'

--- advertisments/app_advertisments/views.py
+++ advertisments/app_advertisments/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render # Для отправки HTML по запросу пользователя
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
+def test(request):
     return HttpResponse('Успешно')
+def index(request):
+    return render(request, "index.html")
+
+def top_sellers(request):
+    return render(request, "top-sellers.html")
